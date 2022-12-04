8bf6b0dc04abd1d444f8c12732daa6075d4b6d82
imalgrab <igormalgrab@gmail.com>
2022-12-05 00:53:27 +0100

    day3 part2

--- day3/index.ts
+++ day3/index.ts
@@ -35,5 +35,48 @@ function solvePartOne(): number {
   }, 0);
 }
 
-const part1 = solvePartOne();
-console.log(`Sum of the priorities is ${part1}`);
+// const part1 = solvePartOne();
+// console.log(`Sum of the priorities is ${part1}`);
+
+function parseInputPartTwo(): string[][] {
+  const inputArray = input.split('\n');
+  const returnArray: string[][] = [];
+  for (let i = 0; i < inputArray.length; i += 3) {
+    const triple = [inputArray[i], inputArray[i + 1], inputArray[i + 2]];
+    returnArray.push(triple);
+  }
+  return returnArray;
+}
+
+function findDuplicatedElementPartTwo(array: string[]): string | undefined {
+  const withoutRepetitions = array.map((line) =>
+    Array.from(new Set(line.split(''))).join('')
+  );
+
+  const combined = withoutRepetitions.join('');
+  const set: Record<string, number> = {};
+
+  combined.split('').forEach((character) => {
+    if (set[character] === undefined) {
+      set[character] = 1;
+    } else {
+      set[character] += 1;
+    }
+  });
+
+  return Object.entries(set).find(([k, v]) => v === 3)?.[0];
+}
+
+function solvePartTwo(): number {
+  const parsedInput = parseInputPartTwo();
+  return parsedInput.reduce((acc, currentArray) => {
+    const duplicatedElement = findDuplicatedElementPartTwo(currentArray);
+    if (duplicatedElement) {
+      return acc + calculatePriority(duplicatedElement);
+    }
+    return acc;
+  }, 0);
+}
+
+const part2 = solvePartTwo();
+console.log(`Sum of the priorities of those item types is ${part2}`);
