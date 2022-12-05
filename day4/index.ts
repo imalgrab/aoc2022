24f28fc830ee627195eea34135a37ad92c48eb9e
imalgrab <igormalgrab@gmail.com>
2022-12-05 12:04:38 +0100

    day4 (add predicate)

--- day4/index.ts
+++ day4/index.ts
@@ -10,32 +10,23 @@ function containsOneAnother(left: string, right: string): boolean {
   return (l1 <= r1 && l2 >= r2) || (r1 <= l1 && r2 >= l2);
 }
 
-function solvePartOne() {
-  const pairs = parseInput();
-  return pairs.reduce((acc, curr) => {
-    const [first, second] = curr;
-    const result = containsOneAnother(first, second);
-    return acc + Number(result);
-  }, 0);
-}
-
-const partOne = solvePartOne();
-console.log(`One range fully contain the other in ${partOne} assignment pairs`);
-
 function overlaps(left: string, right: string): boolean {
   const [l1, l2] = left.split('-').map((x) => parseInt(x));
   const [r1, r2] = right.split('-').map((x) => parseInt(x));
   return (l1 <= r1 && l2 >= r1) || (l1 > r1 && r2 >= l1);
 }
 
-function solvePartTwo() {
+function solve(predicate: (left: string, right: string) => boolean): number {
   const pairs = parseInput();
   return pairs.reduce((acc, curr) => {
     const [first, second] = curr;
-    const result = overlaps(first, second);
+    const result = predicate(first, second);
     return acc + Number(result);
   }, 0);
 }
 
-const partTwo = solvePartTwo();
+const partOne = solve(containsOneAnother);
+console.log(`One range fully contain the other in ${partOne} assignment pairs`);
+
+const partTwo = solve(overlaps);
 console.log(`Ranges overlap in ${partTwo} assignment pairs`);
