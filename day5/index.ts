7151edf1469d52eeaf165c62c8fecf37889253bd
imalgrab <igormalgrab@gmail.com>
2022-12-06 14:53:16 +0100

    day5 (parser improvements)

--- day5/index.ts
+++ day5/index.ts
@@ -1,89 +1,47 @@
 import input from './input';
 
+type Crane = 'CrateMover2000' | 'CrateMover2001';
+
 function parseInput(): [string[][], number[][]] {
-  const data = input.split('\n');
-  const separatorIndex = data.findIndex((x) => x === '');
-
-  const stacks = data
-    .slice(0, separatorIndex)
-    .map((line) => line.split(' '))
-    .reverse();
-
-  const moves = data.slice(separatorIndex + 1).map((move) =>
-    move
+  const data = input.split('\n\n');
+  const [rawStacks, rawMoves] = data;
+  const stacks = rawStacks.split('\n').reverse();
+  const moves = rawMoves.split('\n').map((move) => {
+    return move
       .split(' ')
       .map(Number)
-      .filter((x) => !isNaN(x))
-  );
-
-  const numberOfStacks = stacks[0].filter((x) => x !== '').length;
-  const formattedStacks: string[][] = Array.from(
-    Array(numberOfStacks),
-    () => []
-  );
-
-  const formattedArray: string[][] = Array.from(
-    Array(numberOfStacks),
-    () => []
-  );
-
-  let spaceCount = 0;
-  stacks.slice(1).forEach((line, i) => {
-    line.forEach((character) => {
-      if (character === '' && spaceCount < 3) {
-        spaceCount++;
-      } else if (spaceCount === 3) {
-        formattedArray[i].push('');
-        spaceCount = 0;
-      } else {
-        formattedArray[i].push(character);
-      }
-    });
+      .filter((x) => !isNaN(x));
   });
 
-  formattedArray.forEach((array) => {
-    array.forEach((element, i) => {
-      if (element.length === 3) {
-        const newElem = element[1];
-        formattedStacks[i].push(newElem);
-      }
-    });
-  });
+  const numberOfStacks = stacks[0].split(' ').filter((x) => x !== '').length;
+  const finalStacks: string[][] = Array.from(Array(numberOfStacks), () => []);
 
-  return [formattedStacks, moves];
-}
-
-function solvePartOne() {
-  const [stacks, moves] = parseInput();
-
-  moves.forEach(([quantity, moveFrom, moveTo]) => {
-    for (let i = 0; i < quantity; i++) {
-      const elem = stacks[moveFrom - 1].pop();
-      if (elem) {
-        stacks[moveTo - 1].push(elem);
+  stacks.slice(1).forEach((line) => {
+    for (let i = 0; i < numberOfStacks; i++) {
+      const position = 4 * i + 1;
+      if (line[position] !== ' ') {
+        finalStacks[i].push(line[position]);
       }
     }
   });
-  return stacks.reduce((acc, currentStack) => {
-    return acc + currentStack[currentStack.length - 1];
-  }, '');
+
+  return [finalStacks, moves];
 }
 
-const partOne = solvePartOne();
-console.log(`${partOne}`);
-
-function solvePartTwo() {
+function solve(crane: Crane) {
   const [stacks, moves] = parseInput();
-
   moves.forEach(([quantity, moveFrom, moveTo]) => {
     const block = stacks[moveFrom - 1].splice(-quantity);
+    if (crane === 'CrateMover2000') {
+      block.reverse();
+    }
     stacks[moveTo - 1].push(...block);
   });
-
-  return stacks.reduce((acc, currentStack) => {
-    return acc + currentStack[currentStack.length - 1];
-  }, '');
+  return stacks.reduce((acc, currentStack) => acc + currentStack.pop(), '');
 }
 
-const partTwo = solvePartTwo();
+const partOne = solve('CrateMover2000');
+console.log(`${partOne}`);
+
+const partTwo = solve('CrateMover2001');
 console.log(`${partTwo}`);
